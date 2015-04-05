5807126dcf2c54eacd5aa7baf38d1a6bb90093be
magacoelho <mgacoelho@yahoo.es>
2015-04-05 17:22:13 +0200

    ADD IntervalFactory ADD IntervalFactory.getInterval

---
+++ src/tests/IntervalFactory.java
@@ -0,0 +1,12 @@
+package tests;
+
+import intervals.Opening;
+
+public class IntervalFactory {
+
+	public static Object getInterval(int i, int j, Opening bothOpened) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
