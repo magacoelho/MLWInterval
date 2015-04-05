cd4b71a90055b8d48716b07061820a52a7014564
magacoelho <mgacoelho@yahoo.es>
2015-04-05 17:29:51 +0200

    ADD Interval.includes(Interval)-> Empty

---
+++ src/intervals/Interval.java
@@ -0,0 +1,20 @@
+package intervals;
+
+public class Interval {
+
+	public double midPoint() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public boolean includes(double value) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	public boolean includes(Interval interval) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
