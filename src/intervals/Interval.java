1f80b52d1095608e3e54b7ef46176cb43b14f9a6
magacoelho <mgacoelho@yahoo.es>
2015-04-05 18:39:25 +0200

    OK BOTH_OPENED includes LEFT_OPENED

--- src/intervals/Interval.java
+++ src/intervals/Interval.java
@@ -33,10 +33,13 @@ public class Interval {
 						case BOTH_OPENED: if  ((this.includes(interval.getMin())||this.getMin()==interval.getMin())
 							                    &&((this.includes(interval.getMax())||this.getMax()==interval.getMax())))
 							                      return true;
-						case LEFT_OPENED: break;	            
+						case LEFT_OPENED: if  ((this.includes(interval.getMin())||this.getMin()==interval.getMin())
+			                                  &&((this.includes(interval.getMax()))))
+		                                   return true;
+							              break;	            
 						case RIGHT_OPENED:break;
 						case UNOPENED: break;
-						 default: return false;
+						default: return false;
 	    				}
 			
 			            break;
