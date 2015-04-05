134df44583b7217cd6469c22e9125d1af3fc99d8
magacoelho <mgacoelho@yahoo.es>
2015-04-05 20:21:37 +0200

    UPDATE IntervalFactory.. getInterval

--- src/intervals/IntervalFactory.java
+++ src/intervals/IntervalFactory.java
@@ -6,7 +6,22 @@ public class IntervalFactory {
     private static IntervalLeftOpened intervalleftOpened;
     private static IntervalRightOpened intervalRightOpened;
     private static IntervalUnopened intervalUnopened;
+    public static Interval interval=null;
 	public static Interval getInterval(double min, double max, Opening opening) {
+		switch(opening){
+		case BOTH_OPENED:if(intervalBothOpened==null)
+			                  interval= new IntervalBothOpened(min, max, opening);   
+		case LEFT_OPENED:if(intervalleftOpened==null)
+                            interval= new IntervalLeftOpened(min, max, opening);;
+    	case RIGHT_OPENED: ;
+		case UNOPENED: ;   
+		default: ;
+		
+		
+		
+		}
+		
+		
 		return new Interval(min, max,opening);
 	}
 
