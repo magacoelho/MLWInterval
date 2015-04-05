1e9e11fb877934026822eac930e6382439dce745
magacoelho <mgacoelho@yahoo.es>
2015-04-05 17:48:18 +0200

    UPDATE Interval.includes(double) ADD Interval.equals, hash--> autogenaration

--- src/intervals/Interval.java
+++ src/intervals/Interval.java
@@ -18,7 +18,8 @@ public class Interval {
 
 	public boolean includes(double value) {
 		switch(this.opening){
-		case LEFT_OPENED:break;
+		case LEFT_OPENED:return this.min<value&& this.max>value;
+			            
 		
 		
 		
@@ -42,4 +43,36 @@ public class Interval {
 		return null;
 	}
 
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		long temp;
+		temp = Double.doubleToLongBits(max);
+		result = prime * result + (int) (temp ^ (temp >>> 32));
+		temp = Double.doubleToLongBits(min);
+		result = prime * result + (int) (temp ^ (temp >>> 32));
+		result = prime * result + ((opening == null) ? 0 : opening.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Interval other = (Interval) obj;
+		if (Double.doubleToLongBits(max) != Double.doubleToLongBits(other.max))
+			return false;
+		if (Double.doubleToLongBits(min) != Double.doubleToLongBits(other.min))
+			return false;
+		if (opening != other.opening)
+			return false;
+		return true;
+	}
+	
+    
 }
