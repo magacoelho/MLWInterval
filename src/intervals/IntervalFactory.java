ac7e14afc7d5710af9508eb7dcc992c3ad113497
magacoelho <mgacoelho@yahoo.es>
2015-04-06 13:11:27 +0200

    IntervalFactory-limpieza

--- src/intervals/IntervalFactory.java
+++ src/intervals/IntervalFactory.java
@@ -2,31 +2,24 @@ package intervals;
 
 
 public class IntervalFactory {
-	private static  IntervalBothOpened intervalBothOpened;
-    private static IntervalLeftOpened intervalleftOpened;
-    private static IntervalRightOpened intervalRightOpened;
-    private static IntervalUnopened intervalUnopened;
     public static Interval interval=null;
 	public static Interval getInterval(double min, double max, Opening opening) {
 		switch(opening){
 		case BOTH_OPENED:
-			                  interval= new IntervalBothOpened(min, max); 
-			                  
-		                   break;
+		                 interval= new IntervalBothOpened(min, max); 
+		                  break;
 		case LEFT_OPENED:
-                            interval= new IntervalLeftOpened(min, max);
-		                   break;
+                         interval= new IntervalLeftOpened(min, max);
+		                 break;
     	case RIGHT_OPENED:
-                             interval= new IntervalRightOpened(min, max); 
+                         interval= new IntervalRightOpened(min, max); 
     	                  break;
 		case UNOPENED: 
-                          interval= new IntervalUnopened(min, max);
+                         interval= new IntervalUnopened(min, max);
 		               break;
 		default:  throw new IllegalArgumentException("Incorrect type code value");
 			
 		}
-		
-		
 		return interval;
 	}
 
