83dc82e88f184d82eb077c7735a42565159aace8
magacoelho <mgacoelho@yahoo.es>
2015-04-09 20:39:28 +0200

    add OpenedRightPoint, ClosedRightPoint

---
+++ src/intervals/ClosedRightPoint.java
@@ -0,0 +1,10 @@
+package intervals;
+
+public class ClosedRightPoint extends Point{
+
+	public ClosedRightPoint(double value) {
+		super(value);
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ src/intervals/OpenedRightPoint.java
@@ -0,0 +1,10 @@
+package intervals;
+
+public class OpenedRightPoint extends Point{
+
+	public OpenedRightPoint(double value) {
+		super(value);
+		// TODO Auto-generated constructor stub
+	}
+
+}
