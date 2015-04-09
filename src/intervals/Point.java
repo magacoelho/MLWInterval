20e64dd551c664d0e2c4fc399c5de20b3234532c
magacoelho <mgacoelho@yahoo.es>
2015-04-09 20:08:22 +0200

    ADD Point.toString, equals, hash

--- src/intervals/Point.java
+++ src/intervals/Point.java
@@ -32,5 +32,35 @@ public class Point {
 	public boolean greaterOrEquals(Point p) {
 		return this.greaterThan(p)||this.equalsValues(p);
 	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		long temp;
+		temp = Double.doubleToLongBits(value);
+		result = prime * result + (int) (temp ^ (temp >>> 32));
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Point other = (Point) obj;
+		if (Double.doubleToLongBits(value) != Double
+				.doubleToLongBits(other.value))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "Point [value=" + value + "]";
+	}
 	
 }
