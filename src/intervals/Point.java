81ccd8b5869fbd90ebe427e8aac016c04ff54c0a
magacoelho <mgacoelho@yahoo.es>
2015-04-09 19:46:24 +0200

    forget click teh class Point

---
+++ src/intervals/Point.java
@@ -0,0 +1,17 @@
+package intervals;
+
+public class Point {
+     private double value; 
+	public Point(double value) {
+		this.value=value;
+	}
+
+	public double getValue() {
+		// TODO Auto-generated method stub
+		return this.value;
+	}
+
+	public void setValue(double value) {
+		this.value=value;
+	}
+}
