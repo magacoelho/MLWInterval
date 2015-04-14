473c9c5a2c70750ac851198233becbf577a6109a
magacoelho <mgacoelho@yahoo.es>
2015-04-14 18:59:10 +0200

    test ok-> PointOpened y PointClosed

--- src/intervals/IntervalFactory.java
+++ src/intervals/IntervalFactory.java
@@ -6,16 +6,16 @@ public class IntervalFactory {
 	public static Interval getInterval(double min, double max, Opening opening) {
 		switch(opening){
 		case BOTH_OPENED:
-		                 interval= new IntervalBothOpened(new PointOpened(min), new OpenedRightPoint(max)); 
+		                 interval= new IntervalBothOpened(new PointOpened(min), new PointOpened(max)); 
 		                  break;
 		case LEFT_OPENED:
                          interval= new IntervalLeftOpened(new PointOpened(min),new PointClosed(max));
 		                 break;
     	case RIGHT_OPENED:
-                         interval= new IntervalRightOpened(new ClosedLeftPoint(min), new OpenedRightPoint(max));
+                         interval= new IntervalRightOpened(new PointClosed(min), new PointOpened(max));
     	                  break;
 		case UNOPENED: 
-                         interval= new IntervalUnopened(new ClosedLeftPoint(min), new OpenedRightPoint(max));
+                         interval= new IntervalUnopened(new PointClosed(min), new PointClosed(max));
 		               break;
 		default:  throw new IllegalArgumentException("Incorrect type code value");
 			
